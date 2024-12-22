5ef829ef342aa350e092d13c58c2926d61e3acc3
diogofonseca01 <diogoalexcf@gmail.com>
2024-12-22 22:17:17 +0000

    dashboard with bd data

---
+++ postgresSQL_Scripts/SPs_TRGs_FNs/dashboard_logic_objects.sql
@@ -0,0 +1,46 @@
+-- FUNCTION TO GET OVERVIEW SECTION IN DASHBOARD
+CREATE OR REPLACE FUNCTION get_overview()
+RETURNS TABLE(total_revenue NUMERIC, expected_guests BIGINT) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        SUM(total_value) AS total_revenue,
+        COUNT(DISTINCT client_id) AS expected_guests
+    FROM "RESERVES.reservation"
+    WHERE status = 'C'; -- Only confirmed reservations
+END;
+$$ LANGUAGE plpgsql;
+
+-- FUNCTION TO GET THE TOP PLACEMENTS SECTION IN DASHBOARD
+CREATE OR REPLACE FUNCTION get_top_placements()
+RETURNS TABLE(city VARCHAR, revenue NUMERIC) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        h.city,
+        SUM(r.total_value) AS revenue
+    FROM "RESERVES.reservation" r
+    JOIN "RESERVES.room_reservation" rr ON r.id = rr.reservation_id
+    JOIN "ROOM_MANAGEMENT.room" rm ON rr.room_id = rm.id
+    JOIN "MANAGEMENT.hotel" h ON rm.hotel_id = h.id
+    GROUP BY h.city
+    ORDER BY revenue DESC
+    LIMIT 5;
+END;
+$$ LANGUAGE plpgsql;
+
+-- FUNCTION TO GET THE SALES OVERTIME SECTION IN DASHBOARD
+CREATE OR REPLACE FUNCTION get_sales_over_time()
+RETURNS TABLE(day INTEGER, revenue NUMERIC) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        EXTRACT(DAY FROM r.begin_date)::INTEGER AS day,
+        SUM(r.total_value) AS revenue
+    FROM "RESERVES.reservation" r
+    WHERE r.begin_date >= DATE_TRUNC('month', CURRENT_DATE)
+    GROUP BY day
+    ORDER BY day;
+END;
+$$ LANGUAGE plpgsql;
+
