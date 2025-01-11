073c3242f5dd57032398ba83450400272ef493b0
Alexandre Marques <alexandremarquesrm@gmail.com>
2025-01-11 02:00:17 +0000

    fix url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AdminEmployeeDashboardView.as_view(), name='admin_dashboard'),
-    path('employee/', views.employee_dashboard, name='employee_dash'),
+    path('', views.AdminEmployeeDashboardView.as_view(), name='admin_dashboard')
 ]
