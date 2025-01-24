42f5097485e08def5701624ae3129792a0b99e7e
alexmarquesrm <alexandremarquesrm@gmail.com>
2025-01-24 16:42:22 +0000

    landing page dinamica

--- main/views.py
+++ main/views.py
@@ -3,10 +3,32 @@ from django.http import HttpResponse
 from bson import ObjectId
 from luxehorizon.db_mongo import db
 import gridfs
-
+from django.db.models import Min
+from hotel_management.models import Hotel
+from main.mongo_utils import get_number_of_reviews, get_files_by_postgres_id, upload_file_with_metadata, get_cover_image
 
 def index(request):
-    return render(request, 'index.html')
+    # Busca os 3 hotéis com as maiores avaliações (número de estrelas)
+    top_hotels = (
+        Hotel.objects.annotate(min_price=Min('room__base_price'))  # Calcula o menor preço dos quartos
+        .order_by('-stars')[:3]  # Ordena por estrelas em ordem decrescente e limita a 3 resultados
+    )
+
+    for hotel in top_hotels:
+        num_rev = get_number_of_reviews(hotel.id)
+        file = get_cover_image(hotel.id)
+
+        if file is not None:
+            # Converte o ID do arquivo em uma string
+            file.id_str = str(file._id)
+            hotel.cover_picture = file
+        else:
+            hotel.cover_picture = None
+        hotel.num_reviews = num_rev
+
+    return render(request, 'index.html', {
+        'top_hotels': top_hotels,  # Passa os 3 melhores hotéis para o template
+    })
 
 def serve_image(request, file_id):
     # Get the file object from MongoDB using GridFS
